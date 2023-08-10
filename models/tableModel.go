7671b2fb9f413e4e3b3426d593677715a5e297a6
shnartho <shnartho@gmail.com>
2023-08-10 17:00:07 +0100

    models are done

--- models/tableModel.go
+++ models/tableModel.go
@@ -8,9 +8,9 @@ import (
 
 type Table struct {
 	ID               primitive.ObjectID `bson:"_id"`
-	Number_of_guests *int               `bson:"number_of_guests" validate:"required"`
-	Table_number     *int               `bson:"table_number" validate:"required"`
-	Created_at       time.Time          `bson:"created_at"`
-	Updated_at       time.Time          `bson:"updated_at"`
-	Table_id         string             `bson:"table_id"`
+	Number_of_guests *int               `json:"number_of_guests" validate:"required"`
+	Table_number     *int               `json:"table_number" validate:"required"`
+	Created_at       time.Time          `json:"created_at"`
+	Updated_at       time.Time          `json:"updated_at"`
+	Table_id         string             `json:"table_id"`
 }
